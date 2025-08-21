c84527f5352bf09eec435c70e90dad4b4ff95600
KennySabee <komerizalde@gmail.com>
2025-08-21 13:18:29 -0500

    feat: last update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -50,7 +50,7 @@ const socialLinks: SocialLink[] = [
 const contactInfo: ContactInfo[] = [
   { icon: <PhoneIcon className="w-5 h-5 mr-3 text-yellow-500" />, text: '(+593) 995745004  ' },
   { icon: <MailIcon className="w-5 h-5 mr-3 text-yellow-500" />, text: 'ikids.kindergarden@gmail.com' },
-  { icon: <MapPinIcon className="w-5 h-5 mr-3 text-yellow-500" />, text: 'Av. Alemania y Mariana de Jesús' },
+  { icon: <MapPinIcon className="w-5 h-5 mr-3 text-yellow-500" />, text: 'Av. Telegrafo y Últimas Noticias' },
 ];
 
 export const Footer = () => {
